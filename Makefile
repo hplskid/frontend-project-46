80ed87a0fe2a30b8b458c81c915ed1c028cf11d0
hplskid <hplskid@gmail.com>
2023-01-30 11:09:41 +0300

    The initial CLI structure

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	npm ci
+
+link:
+	npm link
---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program.version("1.0.0")
+  .usage("[options] <filepath1> <filepath2>")
+  .description("Compares two configuration files and shows a difference.");
+
+program.option('-f, --format <type>', 'output format');
+
+program.parse(process.argv);
